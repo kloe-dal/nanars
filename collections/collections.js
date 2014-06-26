47ad190558d46d196b478c3b351f799aea93fa1e
kloe-dal <kloe.dal@gmail.com>
2014-06-26 14:19:03 +0200

    Début de mise en page

--- collections/collections.js
+++ collections/collections.js
@@ -9,4 +9,8 @@ Books = [
     title: "La soupe au céléri",
     resume: "ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
   },
+  { img: "/images/livre3.jpg",
+    title: "La soupe au radis",
+    resume: "ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
+  },
 ];
