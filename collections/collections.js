ccdf86efdf828202643f01967147e83273adec5c
kloe-dal <kloe.dal@gmail.com>
2014-07-01 23:14:31 +0200

    mise en page

--- collections/collections.js
+++ collections/collections.js
@@ -2,15 +2,19 @@ Emails = new Meteor.Collection("emails");
 
 Books = [
   { img: "/images/livre1.jpg",
-    title: "La soupe au choux",
+    title: "L'ennemi dans l'ombre",
     resume: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
   },
   { img: "/images/livre2.jpg",
-    title: "La soupe au céléri",
+    title: "Le démon des âges troubles",
     resume: "ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
   },
   { img: "/images/livre3.jpg",
-    title: "La soupe au radis",
+    title: "Le zénith... Et après?",
+    resume: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
+  },
+  { img: "/images/livre4.jpg",
+    title: "L'autoroute sauvage",
     resume: "ipsum dolor sit amet, consectetur adipisicing elit. Dolore id natus tenetur rerum dolores odit vel cumque eos aperiam, dolorem possimus dolorum iure dicta inventore recusandae quam itaque obcaecati sint."
   },
 ];
